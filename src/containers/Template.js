ed080b9b4a260da9321c88b32541502e3d44413c
William-Cruz <chiefxcruz@gmail.com>
2018-09-06 19:41:24 -0500

    third commit

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react'
 import MuiTHemeProvider from 'material-ui/styles/MuiThemeProvider'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import RaisedButton from 'material-ui/RaisedButton'
-
+import NavDrawer from '../components/NavDrawer'
 injectTapEventPlugin()
 
 class Template extends Component {
@@ -11,14 +10,7 @@ class Template extends Component {
         return (
             <MuiTHemeProvider>
                 <div>
-                    <header>
-                        <h1>TicTacTuring</h1>
-                        <RaisedButton
-                            label={'test button'}
-                            primary={true}
-                            onClick={()=>{console.log('hello I work')}}
-                        /> 
-                    </header> 
+                    <NavDrawer/>
                     <main> 
                         {this.props.children} 
                     </main> 
